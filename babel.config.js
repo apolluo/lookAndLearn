b287155f206cc355201ef9ebd2634beed08775c2
mingluo <mingluo@tencent.com>
2022-03-10 18:13:11 +0800

    'feat:引入antd'

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@icons': './src/icons',
+          '@styles': './src/styles',
+          '@components': './src/components',
+          '@pages': './src/pages',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    ['import', {libraryName: '@ant-design/react-native'}],
+  ],
 };
